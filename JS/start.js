49bd000ee39fb0de6fbc2821f2dce4be08c6cc02
lukeiowa <84240446+lukeiowa@users.noreply.github.com>
2021-05-17 08:12:29 -0400

    Update start.js

--- JS/start.js
+++ JS/start.js
@@ -23,3 +23,59 @@ function display() {
   message += "<li><b>PASSWORD: </b>" + document.login.psw.value + "</ul>";
   DispWin.document.write(message);
 }
+
+
+
+function openModal() {
+document.getElementById('id01').style.display = 'block';
+if (localStorage.getItem("rememberme") == "true") {
+        document.getElementById("uname").value =
+localStorage.getItem("username");
+    document.getElementById("psw").value =
+localStorage.getItem("password");
+} }
+function setUserInfo() {
+        var username = document.getElementById("uname").value;
+    var password = document.getElementById("psw").value;
+    var checkbox = document.getElementById("remember").checked;
+  if (localStorage.getItem("loggedin") != "true") {
+        if (username == "" || password == "") {
+    window.alert("Please fill out the form!");
+    } else {
+    window.alert("Welcome " + username + "!\n Your Username is : " +
+username + "\nYour Password is: " + password);
+    if (checkbox == true) {
+    window.alert("Remember me is on!");
+    localStorage.setItem("rememberme", "true");
+    }
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    localStorage.setItem("loggedin", "true");
+  location.replace("https://lukeiowa.github.io/pages/home.html");
+    }
+} else {
+  if (checkbox == true) {
+    window.alert("Remember me is on!");
+    localStorage.setItem("rememberme", "true");
+    }
+  if (username != localStorage.getItem("username") && password !=
+localStorage.getItem("password")) {
+   window.alert("Incorrect Username and Password!");
+        } else if (username != localStorage.getItem("username")) {
+    window.alert("Incorrect Username!");
+    } else if (password != localStorage.getItem("password")) {
+    window.alert("Incorrect Password!");
+ 
+} else {
+        window.alert("Welcome back " + username + "!");
+     location.replace("https://lukeiowa.github.io/pages/home.html");
+} }
+}
+function forgotPassword() {
+if (localStorage.getItem("loggedin") == "true") {
+        window.alert("Your Password is: " +
+localStorage.getItem("password"));
+} else {
+ }
+window.alert("Sign up to see this option!");
+}
