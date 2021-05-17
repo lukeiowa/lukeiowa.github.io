6a7224b53d2cdb012a4b6d0249db478f5a2e67a8
lukeiowa <84240446+lukeiowa@users.noreply.github.com>
2021-05-17 05:47:24 -0400

    Update start.js

--- JS/start.js
+++ JS/start.js
@@ -6,7 +6,9 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-  
+ 
+
+
 function directToHomepage() {
   location.replace("https://lukeiowa.github.io/pages/home.html");
   window.alert("kill me");
@@ -18,19 +20,4 @@ function display() {
   message = "<ul><li><b>USERNAME: </b>" + document.login.uname.value;
   message += "<li><b>PASSWORD: </b>" + document.login.psw.value + "</ul>";
   DispWin.document.write(message);
-}
-
-function setUserInfo() {
-    var username = document.getElementById("uname").value;
-    var password = document.getElementById("psw").value;
-    if (username == "" || password == "") {
-    window.alert("Please fill out the form!");
-    } else {
-    window.alert("Welcome " + username + "!\n Your Username is : " + username + "\nYour Password is: " + password); 
-    
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
-    localStorage.setItem("loggedin", "true");
-    location.replace("https://lukeiowa.github.io/pages/home.html");
-    }
 }
