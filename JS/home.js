6144fa40b9728a304ffec3fd2f763d03c3486419
lukeiowa <84240446+lukeiowa@users.noreply.github.com>
2021-05-18 09:39:51 -0400

    Update home.js

--- JS/home.js
+++ JS/home.js
@@ -1,7 +1,18 @@
+window.onload = function startPage() {
+    redirect();
+}
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+}
+
+function redirect() {
+if (localStorage.getItem("loggedin") == null) {
+        window.alert("Hello! You have not selected a payment plan!\nDirecting you to our main page...");
+        location.replace("https://lukeiowa.github.io/pages/start.html");
+    }
 }
