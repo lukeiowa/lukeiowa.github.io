725b44968f4182f1675c6bcbc907d085e86dff13
lukeiowa <84240446+lukeiowa@users.noreply.github.com>
2021-05-20 19:48:07 -0400

    Create bfd.js

---
+++ JS/bfd.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function redirect() {
+        location.replace("https://lukeiowa.github.io/game.html");
+}
