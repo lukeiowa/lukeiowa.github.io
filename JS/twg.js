c8e6583bcd6f2f1f1f00734ff03c932ab2507896
lukeiowa <84240446+lukeiowa@users.noreply.github.com>
2021-05-21 06:23:38 -0400

    Update twg.js

--- JS/twg.js
+++ JS/twg.js
@@ -1,5 +1,30 @@
 var d = new Date("2015-03-25");
 
 window.onload = function startingPage() {
-  window.alert("NOTE: The game is currently down as of " + d + " but will be fixed shortly (Around 3PM EST?)");
+  window.alert("NOTE: The generator is currently down as of " + d + " but will be fixed shortly (Around 3PM EST?)");
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function redirect() {
+        location.replace("https://lukeiowa.github.io/generator.html");
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var a;
+for (a = 0; a < coll.length; a++) {
+  coll[a].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+}); }
