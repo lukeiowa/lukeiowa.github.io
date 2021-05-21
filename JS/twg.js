e6eac1fda783cd692f41755f6552913189c361c6
lukeiowa <84240446+lukeiowa@users.noreply.github.com>
2021-05-21 06:17:42 -0400

    Create twg.js

---
+++ JS/twg.js
@@ -0,0 +1,5 @@
+var d = new Date("2015-03-25");
+
+window.onload = function startingPage() {
+  window.alert("NOTE: The game is currently down as of " + d + " but will be fixed shortly (Around 3PM EST?)");
+}
